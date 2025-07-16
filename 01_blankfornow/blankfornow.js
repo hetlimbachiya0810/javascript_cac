4077a235cf71f2349a07ac930d448f2968456d7f
HETLimbachiya <139550333+hetlimbachiya0810@users.noreply.github.com>
2025-07-16 09:47:47 +0000

    First commit

---
+++ 01_blankfornow/blankfornow.js
@@ -0,0 +1,21 @@
+console.log("Knock knowk mf")
+
+let boolScore = 0;
+console.log("Here we assigned type [" + typeof boolScore + "] : " + boolScore);
+
+// if (typeof boolScore === "boolean") {
+//     console.log("boolScore is a boolean: " + boolScore);
+//     if (boolScore === true) {
+//         boolScore = 1;
+//     }
+//     else {boolScore = 0;}
+//     console.log("boolScore is now an int: " + boolScore);
+   
+// }
+
+let bool2Num = Boolean(boolScore)
+console.log("bool2Num is now a [" + typeof bool2Num + "] : " + bool2Num);
+
+
+
+
